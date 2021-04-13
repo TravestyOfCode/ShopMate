5b5e7d75976093f97f1e855d1303088f0d0ca894
travis@travestyofcode.com <travis@travestyofcode.com>
2021-04-13 16:29:33 -0400

    Adds Identity pages.

---
+++ ShopMate.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopMate.Data;
+
+[assembly: HostingStartup(typeof(ShopMate.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ShopMate.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
