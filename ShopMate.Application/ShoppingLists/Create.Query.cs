d1c92b47b3c874c54a2d6774ad2429dc586ee0ec
travis@travestyofcode.com <travis@travestyofcode.com>
2021-04-15 08:55:00 -0400

    Updates ShoppingList create model and page.

--- ShopMate.Application/ShoppingLists/Create.Query.cs
+++ ShopMate.Application/ShoppingLists/Create.Query.cs
@@ -1,5 +1,9 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ShopMate.Data;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,17 +23,65 @@ namespace ShopMate.Application.ShoppingLists
             public DateTime TripDate { get; set; }
 
             public string Store { get; set; }
+
+            public List<ShoppingListItem> Items { get; set; }
+
+            public Dictionary<int, string> Products { get; set; }
+
+            public Dictionary<int, string> UnitSizes { get; set; }
+
+            public class ShoppingListItem
+            {
+                public int ProductId { get; set; }
+
+                public int ProductDefaultUnitSizeId { get; set; }
+
+                public decimal Quantity { get; set; }
+
+                public int UnitSizeId { get; set; }
+            }
+
         }
 
         public class QueryHandler : IRequestHandler<Query, Model>
-        {            
+        {
+            private readonly ApplicationDbContext _dbContext;
+
+            private readonly ILogger<QueryHandler> _logger;
+
+            public QueryHandler(ApplicationDbContext dbContext, ILogger<QueryHandler> logger)
+            {
+                _dbContext = dbContext;
+
+                _logger = logger;
+            }
+
             public QueryHandler()
             {                
             }
 
-            public Task<Model> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
             {
-                return Task.FromResult(new Model());
+                try
+                {
+                    Model result = new Model();
+
+                    result.Items = new List<Model.ShoppingListItem>();
+
+                    result.Products = await _dbContext.Products
+                        .ToDictionaryAsync(p => p.Id, p => p.Name, cancellationToken);
+
+                    result.UnitSizes = await _dbContext.UnitSizes
+                        .ToDictionaryAsync(p => p.Id, p => p.Name, cancellationToken);
+
+                    return result;
+                }
+                catch(Exception ex)
+                {
+                    _logger.LogError(ex, "Unexpected error when handling ShoppingLists.Create.Query with request: {request}", request);
+                    throw;
+                }
+
             }
         }
     }
